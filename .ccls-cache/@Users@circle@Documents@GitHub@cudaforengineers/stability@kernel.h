a9b62fef58d7f944e8562a2fa8a926723a52364f
yuanlq <yuanlq@pku.edu.cn>
2022-05-25 10:38:06 +0800

    delete

--- .ccls-cache/@Users@circle@Documents@GitHub@cudaforengineers/stability@kernel.h
+++
@@ -1,8 +0,0 @@
-#ifndef KERNEL_H
-#define KERNEL_H
-
-struct uchar4;
-
-void kernelLauncher(uchar4 *d_out, int w, int h, float p, int s);
-
-#endif
--- sharpen/main.cpp
+++ sharpen/main.cpp
@@ -4,35 +4,36 @@
 #include <cuda_runtime.h>
 #include <stdlib.h>
 
-int main() {
-  cimg_library::CImg<unsigned char> image("butterfly.bmp");
-  const int w = image.width();
-  const int h = image.height();
+int main()
+{
+   cimg_library::CImg<unsigned char> image("butterfly.bmp");
+   const int w = image.width();
+   const int h = image.height();
 
-  // Initialize uchar4 array for image processing
-  uchar4 *arr = (uchar4 *)malloc(w * h * sizeof(uchar4));
+   // Initialize uchar4 array for image processing
+   uchar4* arr = (uchar4*)malloc(w * h * sizeof(uchar4));
 
-  // Copy CImg data to array
-  for (int r = 0; r < h; ++r) {
-    for (int c = 0; c < w; ++c) {
-      arr[r * w + c].x = image(c, r, 0);
-      arr[r * w + c].y = image(c, r, 1);
-      arr[r * w + c].z = image(c, r, 2);
-    }
-  }
+   // Copy CImg data to array
+   for (int r = 0; r < h; ++r) {
+	  for (int c = 0; c < w; ++c) {
+		 arr[r * w + c].x = image(c, r, 0);
+		 arr[r * w + c].y = image(c, r, 1);
+		 arr[r * w + c].z = image(c, r, 2);
+	  }
+   }
 
-  sharpenParallel(arr, w, h);
+   sharpenParallel(arr, w, h);
 
-  // Copy from array to CImg data
-  for (int r = 0; r < h; ++r) {
-    for (int c = 0; c < w; ++c) {
-      image(c, r, 0) = arr[r * w + c].x;
-      image(c, r, 1) = arr[r * w + c].y;
-      image(c, r, 2) = arr[r * w + c].z;
-    }
-  }
+   // Copy from array to CImg data
+   for (int r = 0; r < h; ++r) {
+	  for (int c = 0; c < w; ++c) {
+		 image(c, r, 0) = arr[r * w + c].x;
+		 image(c, r, 1) = arr[r * w + c].y;
+		 image(c, r, 2) = arr[r * w + c].z;
+	  }
+   }
 
-  image.save_bmp("out.bmp");
-  free(arr);
-  return 0;
+   image.save_bmp("out.bmp");
+   free(arr);
+   return 0;
 }
