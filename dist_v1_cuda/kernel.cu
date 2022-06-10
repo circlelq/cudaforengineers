15ec092289ca3982a60aa39c8527cc02b83ca149
yuanlq <yuanlq@pku.edu.cn>
2022-06-10 15:01:21 +0800

    format

--- dist_v1_cuda/kernel.cu
+++ dist_v1_cuda/kernel.cu
@@ -2,31 +2,37 @@
 #define N 64
 #define TPB 32
 
-__device__ float scale(int i, int n) { return ((float)i) / (n - 1); }
-
-__device__ float distance(float x1, float x2) {
-  return sqrt((x2 - x1) * (x2 - x1));
+__device__ float scale(int i, int n)
+{
+    return ((float)i) / (n - 1);
 }
 
-__global__ void distanceKernel(float *d_out, float ref, int len) {
-  const int i = blockIdx.x * blockDim.x + threadIdx.x;
-  const float x = scale(i, len);
-  d_out[i] = distance(x, ref);
-  printf("i = %2d: dist from %f to %f is %f.\n", i, ref, x, d_out[i]);
+__device__ float distance(float x1, float x2)
+{
+    return sqrt((x2 - x1) * (x2 - x1));
 }
 
-int main() {
-  const float ref = 0.5f;
+__global__ void distanceKernel(float *d_out, float ref, int len)
+{
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    const float x = scale(i, len);
+    d_out[i] = distance(x, ref);
+    printf("i = %2d: dist from %f to %f is %f.\n", i, ref, x, d_out[i]);
+}
 
-  // Declare a pointer for an array of floats
-  float *d_out = 0;
+int main()
+{
+    const float ref = 0.5f;
 
-  // Allocate device memory to store the output array
-  cudaMalloc(&d_out, N * sizeof(float));
+    // Declare a pointer for an array of floats
+    float *d_out = 0;
 
-  // Launch kernel to compute and store distance values
-  distanceKernel<<<N / TPB, TPB>>>(d_out, ref, N);
+    // Allocate device memory to store the output array
+    cudaMalloc(&d_out, N * sizeof(float));
 
-  cudaFree(d_out); // Free the memory
-  return 0;
+    // Launch kernel to compute and store distance values
+    distanceKernel<<<N / TPB, TPB>>>(d_out, ref, N);
+
+    cudaFree(d_out); // Free the memory
+    return 0;
 }
--- dist_v2_cuda/main.cpp
+++ dist_v2_cuda/main.cpp
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 #define N 64
 
-float scale(int i, int n) { return ((float)i) / (n - 1); }
+float scale(int i, int n)
+{
+   return ((float)i) / (n - 1);
+}
 
-int main() {
-  const float ref = 0.5f;
+int main()
+{
+   const float ref = 0.5f;
 
-  float *in = (float *)calloc(N, sizeof(float));
-  float *out = (float *)calloc(N, sizeof(float));
+   float* in = (float*)calloc(N, sizeof(float));
+   float* out = (float*)calloc(N, sizeof(float));
 
-  // Compute scaled input values
-  for (int i = 0; i < N; ++i) {
-    in[i] = scale(i, N);
-  }
+   // Compute scaled input values
+   for (int i = 0; i < N; ++i) {
+	  in[i] = scale(i, N);
+   }
 
-  // Compute distances for the entire array
-  distanceArray(out, in, ref, N);
+   // Compute distances for the entire array
+   distanceArray(out, in, ref, N);
 
-  free(in);
-  free(out);
-  return 0;
+   free(in);
+   free(out);
+   return 0;
 }
